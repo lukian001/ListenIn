d61fdf099c9d177529bf0977ef8e5b0677900f86
slucian001 <lucian.streulea02@gmail.com>
2022-06-02 11:21:46 +0300

    merge?

--- ListenIn/settings.py
+++ ListenIn/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
